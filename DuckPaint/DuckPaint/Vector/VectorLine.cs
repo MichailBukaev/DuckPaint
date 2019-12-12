66fbc3eb8e71aa05c40929e4b27bbd614d0d12b1
Asya19 <asya.kisko@yandex.ru>
2019-12-12 16:48:46 +0300

    add class VectorLine, VectorLineFactory

---
+++ DuckPaint/DuckPaint/Vector/VectorLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckPaint
+{
+    [Serializable]
+    public abstract class VectorLine : VectorFigure
+    {
+       
+        public VectorLine(Point p, Color c, int s)
+        {
+            this.points = new List<Point>();
+            this.Color = c;
+            this.size = s;
+        }
+
+       
+        public override void MouseMoveTillCreation(Point p)
+        {
+            points[1] = p;
+        }
+
+
+
+    }
+}
